75ccf8d6d4ade830ddc41ba5e086332293b74c36
DenisseFerreira <ferreira.denisse@gmail.com>
2019-10-09 16:12:19 -0300

    Interaccion con HTML, en funcionamiento inputs y boton de busqueda

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 class Fetcher {
     // Variables
-    urlBase = '';
-    limit = '';
+    //urlBase = '';
+    limit = '&limit=';
     startDate = '';
     endDate = '';
-    
+
     constructor(urlBase) {
         this.urlBase = urlBase;
     }
 
     search(startDate, endDate, limit) {
         this.urlConsult = this.urlBase + startDate + endDate + limit;
+        console.log(this.urlConsult);
         fetch(this.urlConsult)
             .then(res => res.json()) //respuesta con json
             .then(data => {
@@ -23,22 +24,42 @@ class Fetcher {
             });
     }
 }
+
+
 //Instancias
 let fetcher = new Fetcher('https://api.fda.gov/drug/event.json?search=');
-fetcher.search('[20040101+TO', '+20081231]', '&limit=10');
+
+document.getElementById('btnSearch').addEventListener('click', () => {
+    let startDate = document.getElementById('startDate').value;
+    startDate = startDate.replace(/-+/g, '')
+    console.log('fecha incio es: ', startDate);
+    let endDate = document.getElementById('endDate').value;
+    endDate = endDate.replace(/-+/g, '');
+    console.log('fecha fin es: ', endDate);
+    let valueLimit = document.getElementById('inputLimit').value;
+    console.log('input funciona : ', valueLimit);
+
+    fetcher.search('['+startDate+'+TO', '+'+endDate+ ']', '&limit='+valueLimit);
+})
+
 //console.log(fetcher.search());
+
+
 
 //document.getElementById('itemResult').innerHTML = `${JSON.stringify(fetcher.search())}`;
 
-/* class DomWritter { //Clase para manejar el DOM
-    
-    element;
+
+
+class DomWritter { //Clase para manejar el DOM
+
+    constructor(){}
+ /*    element;
 
     constructor(id) {
         this.element = document.getElementById(id);
         console.log('mostrando a element ', this.element);
-    }
+    } */
 
 
 
-} */
+}
